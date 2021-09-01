fcdd3a9b5178b4cec39cc1bf0428be046ac7afbf
Alex Schlamp <schlamp@kelleroffers.com>
2021-08-31 23:22:42 -0500

    project update for nextjs serverless

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 module.exports = {
   target: "serverless",
-  reactStrictMode: true,
   env: {
     HC_API_SECRET: process.env.HC_API_SECRET,
     HC_API_KEY: process.env.HC_API_KEY,
+    NEXT_PUBLIC_GOOGLE_MAPS_API: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API,
+    NEXT_PUBLIC_GTM_ID: process.env.NEXT_PUBLIC_GTM_ID,
+    NEXT_PUBLIC_GA_TRACKING_ID: process.env.NEXT_PUBLIC_GA_TRACKING_ID,
+    NEXT_PUBLIC_UA_TRACKING_ID: process.env.NEXT_PUBLIC_UA_TRACKING_ID,
   },
 };
