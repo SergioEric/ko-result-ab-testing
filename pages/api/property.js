73d2c1ec27babfd9ea7c2f5fab687db6606d2eb8
Alex Schlamp <schlamp@kelleroffers.com>
2021-09-02 17:03:21 -0500

    loging

--- pages/api/property.js
+++ pages/api/property.js
@@ -8,7 +8,7 @@ const API_SECRET = process.env.HC_API_SECRET;
 
 export default async function handler(req, res) {
   console.log('property: ', req);
-  
+
   const { address, zipcode } = req.query;
   if (address == undefined && zipcode == undefined)
     return res.status(400).send("Address and Zipcode is missing");
@@ -20,32 +20,38 @@ export default async function handler(req, res) {
   params.append("address", address);
   params.append("zipcode", zipcode);
 
-  const response = await fetch(url + "?" + params, {
-    headers: {
-      Authorization:
-        "Basic " + Buffer.from(API_KEY + ":" + API_SECRET).toString("base64"),
-    },
-  });
-  if (response.status != 200) {
-    // not result
-    return no_result(res);
-  }
-  const json = await response.json();
+  try {
+    const response = await fetch(url + "?" + params, {
+      headers: {
+        Authorization:
+          "Basic " + Buffer.from(API_KEY + ":" + API_SECRET).toString("base64"),
+      },
+    });
+    if (response.status != 200) {
+      // not result
+      return no_result(res);
+    }
+    const json = await response.json();
 
-  const property =
-    json.length >= 1 ? json[0]["property/value"] : json["property/value"];
-  if (property.api_code != 0 || property.result == null) {
-    // there's no result for the address
-    return no_result(res);
+    const property =
+      json.length >= 1 ? json[0]["property/value"] : json["property/value"];
+    if (property.api_code != 0 || property.result == null) {
+      // there's no result for the address
+      return no_result(res);
+    }
+    console.log(property);
+    const map = {
+      data: true,
+      price: property.result.value.price_mean,
+      lwr: property.result.value.price_lwr,
+      upr: property.result.value.price_upr,
+    };
+    return res.status(200).json(map);
+  } catch (err) {
+    console.log(err);
   }
-  console.log(property);
-  const map = {
-    data: true,
-    price: property.result.value.price_mean,
-    lwr: property.result.value.price_lwr,
-    upr: property.result.value.price_upr,
-  };
-  return res.status(200).json(map);
+
+  return req.status(400).send("Failed fetch api data");
 }
 
 const no_result = (res) => res.status(200).json({ data: false });
